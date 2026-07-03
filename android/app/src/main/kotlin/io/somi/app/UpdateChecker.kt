b6a87818e8646849eadadb926fb7964d40e5227f
Labushuya <webdevelopment@mindsources.net>
2026-07-04 00:46:15 +0200

    fix: ChannelResult.getOrNull() statt getOrElse

--- android/app/src/main/kotlin/io/somi/app/UpdateChecker.kt
+++ android/app/src/main/kotlin/io/somi/app/UpdateChecker.kt
@@ -168,7 +168,7 @@ object UpdateChecker {
                     // Check for receiver signal (non-blocking).
                     val done = doneChannel.tryReceive()
                     if (done.isSuccess) {
-                        val success = done.getOrElse { false }
+                        val success = done.getOrNull() ?: false
                         emit(if (success) DownloadState.Progress(100) else DownloadState.Failed)
                         emit(if (success) DownloadState.Done else DownloadState.Failed)
                         terminated = true
